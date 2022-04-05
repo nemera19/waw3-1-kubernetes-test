f667838bec5ee5d3044ba082de4079aec21afa63
wwrotniak <85996476+wwrotniak@users.noreply.github.com>
2022-04-05 15:17:29 +0200

    Update get_rst_files.py

--- source/get_rst_files.py
+++ source/get_rst_files.py
@@ -31,17 +31,13 @@ def get_files(urls_list: list) -> dict:
         gh = Github()
         repo = gh.get_repo(repo_name)
         contents_list = repo.get_contents(dir_name)
-        github_raw_url = "https://raw.githubusercontent.com"
+        github_raw = url.replace("github", "raw.githubusercontent").replace("blob", "tree")
         for content in contents_list:
             if content.type == "file":
                 if ".rst" in content.name:
                     repos_url[content.name.split('.')[0]] = content.html_url
                 github_file_url = (
-                    github_raw_url
-                    + "/"
-                    + repo.full_name
-                    + "/master/"
-                    + dir_name
+                    github_raw.replace("tree/", "")
                     + "/"
                     + content.path.replace(" ", "%20").split("/")[-1]
                 )
